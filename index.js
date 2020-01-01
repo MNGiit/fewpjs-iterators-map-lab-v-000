c65789fcd9fa832724670b19dca649fd08031607
Mohammed Noori <kmanoorib@gmail.com>
2020-01-01 06:51:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const tutorials = [
 ];
 
 // https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
-// function titleCased()
-// const titleCased = () =>
+// function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+// }
 
 const titleCased = () => {
   // return tutorials
   // word[0] === word[0].toUpperCase()
   return tutorials.map(function (tutorial) {
-    tutorial = tutorial.split(' ');
-    for ()
+    tutorial = tutorial.charAt(0).toUpperCase() + tutorial.slice(1);
   })
 }
 
